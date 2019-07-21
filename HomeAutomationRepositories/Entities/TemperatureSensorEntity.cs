3c8c9e62f9f67d32a24bfd8e1b09e89cbe4cdd58
Darren Swan <thegundamguru@outlook.com>
2019-07-21 00:00:33 -0500

    tracking changes

---
+++ HomeAutomationRepositories/Entities/TemperatureSensorEntity.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeAutomationRepositories.Entities
+{
+    [BsonDiscriminator("temperaturesensorentity")]
+    public class TemperatureSensorEntity : DeviceEntity
+    {
+        public float Temp { get; set; }
+        public float Humidity { get; set; }
+    }
+}

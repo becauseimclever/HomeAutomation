02d62f759251a476e81d90d94f62d8762cd42798
Darren Swan <thegundamguru@outlook.com>
2020-01-25 23:02:31 -0600

    Updates mostly adding kestrel configuration

---
+++ AutomationWebApi/Extensions/ServiceExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BecauseImClever.HomeAutomation.AutomationWebApi.Extensions
+{
+	public static class ServiceExtensionMethods
+	{
+		public static IServiceCollection AddKestrel(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.Configure<KestrelServerOptions>(configuration.GetSection("Kestrel"));
+
+			return services;
+		}
+	}
+}

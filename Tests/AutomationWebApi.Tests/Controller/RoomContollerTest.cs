2efee35d0758b4d11270b484919ccbdca317dc2d
Darren Swan <fortinbra@becauseimclever.com>
2020-02-07 23:54:58 -0600

    adding more tests

---
+++ Tests/AutomationWebApi.Tests/Controller/RoomContollerTest.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture;
+using BecauseImClever.HomeAutomation.AutomationLogic.Services;
+using BecauseImClever.HomeAutomation.AutomationWebApi.Controllers;
+using Moq;
+using System;
+using Xunit;
+
+namespace BecauseImClever.HomeAutomation.AutomationWebApi.Tests.Controller
+{
+    public class RoomContollerTest
+    {
+        private readonly Fixture _fixture;
+        private readonly Mock<RoomService> _mockRoomService;
+
+        public RoomContollerTest()
+        {
+            _fixture = new Fixture();
+            _mockRoomService = new Mock<RoomService>();
+        }
+        [Fact]
+        public void CreateRoomController()
+        {
+            var controller = new RoomController(_mockRoomService.Object);
+            Assert.NotNull(controller);
+        }
+        [Fact]
+        public void CreateRoomControllerThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => { new RoomController(null); });
+        }
+    }
+}

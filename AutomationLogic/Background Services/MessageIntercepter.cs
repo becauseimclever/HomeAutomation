c08560d65706e3598c2cb6299dba84a527e4d2fe
Darren Swan <fortinbra@becauseimclever.com>
2020-02-09 19:32:06 -0600

    The beginnings of the topic router

--- AutomationLogic/Background Services/MessageIntercepter.cs
+++ AutomationLogic/Background Services/MessageIntercepter.cs
@@ -14,26 +14,34 @@ namespace BecauseImClever.HomeAutomation.AutomationLogic.BackgroundServices
     {
         public Task InterceptApplicationMessagePublishAsync(MqttApplicationMessageInterceptorContext context)
         {
+            var applicationMessage = (context ?? throw new ArgumentNullException(nameof(context))).ApplicationMessage;
+
             var assemblies = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes())
                 .Where(x => typeof(IDevicePlugin).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
-                .Select(x => x.Name).ToList();
+                .Select(x => x.FullName).ToList();
+
             foreach (var assembly in assemblies)
-            {
                 Console.WriteLine(assembly);
+
+            var splitTopic = applicationMessage.Topic.Split('/');
+            var getTopLevel = splitTopic?[0];
+
+            var service = Type.GetType(assemblies.FirstOrDefault(x => x.Contains(getTopLevel, StringComparison.InvariantCultureIgnoreCase)));
+            if (service != null)
+            {
+                var newInstance = (IDevice)Activator.CreateInstance(service);
+                newInstance.Invoke(applicationMessage.Topic, Encoding.UTF8.GetString(applicationMessage.Payload));
+
             }
+
 #pragma warning disable CA1303 // Do not pass literals as localized parameters
             Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
 #pragma warning restore CA1303 // Do not pass literals as localized parameters
-#pragma warning disable CA1062 // Validate arguments of public methods
-            Console.WriteLine($"+ Topic = {context.ApplicationMessage.Topic}");
-#pragma warning restore CA1062 // Validate arguments of public methods
-            Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(context.ApplicationMessage.Payload)}");
-            Console.WriteLine($"+ QoS = {context.ApplicationMessage.QualityOfServiceLevel}");
-            Console.WriteLine($"+ Retain = {context.ApplicationMessage.Retain}");
+            Console.WriteLine($"+ Topic = {applicationMessage.Topic}");
+            Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(applicationMessage.Payload)}");
             Console.WriteLine();
 
             return Task.CompletedTask;
-
         }
     }
 }

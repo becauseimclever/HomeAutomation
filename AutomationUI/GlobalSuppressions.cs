ec23bf1de27f3a0912d226db941403aaede67258
Darren <thegundamguru@yahoo.com>
2020-04-19 23:27:28 -0500

    Removing blazor files for now too

--- AutomationUI/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:BecauseImClever.HomeAutomation.AutomationUI.Startup.Configure(Microsoft.AspNetCore.Components.Builder.IComponentsApplicationBuilder)")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:BecauseImClever.HomeAutomation.AutomationUI.Startup.ConfigureServices(Microsoft.Extensions.DependencyInjection.IServiceCollection)")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:BecauseImClever.HomeAutomation.AutomationUI.Startup.ConfigureServices")]
